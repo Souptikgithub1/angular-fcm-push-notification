14f5cba95b725b0a2de271e78898aea0cc0c019e
Souptik Karmakar <Souptik Karmakar>
2019-03-14 20:54:47 +0530

    change in server.js file

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const path = require('path');
 
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular-fcm-push-notification'));
 
 app.listen(process.env.PORT || 8080);
 
 //pathLocationStrategy
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/angular-fcm-push-notification/index.html'));
 });
 
 console.log('console listening');
